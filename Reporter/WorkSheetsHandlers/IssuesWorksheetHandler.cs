5a7f3e155841a2b2bd53329704034234ed09fd51
d.sizova <daryasa1991@gmail.com>
2024-10-11 18:12:35 +0300

    Убраны лишние поля

--- Reporter/WorkSheetsHandlers/IssuesWorksheetHandler.cs
+++ Reporter/WorkSheetsHandlers/IssuesWorksheetHandler.cs
@@ -25,14 +25,13 @@ public class IssuesWorksheetHandler : WorksheetExportHandlerBase
     private int PriorityColumn = 7;
     private int EstimateTimeColumn = 8;
     //private int TimeLeftColumn = 9;
-    private int TimeSpentColumn = 10;
-    private int StoryPointsColumn = 11;
-    private int CountOfReworkColumn = 12;
-    private int ResolutionColumn = 13;
-    private int ResolutionDateColumn = 14;
-    private int CreateDateColumn = 15;
-    private int UpdateDateColumn = 16;
-
+    //private int TimeSpentColumn = 10;
+    private int StoryPointsColumn = 9;
+    private int CountOfReworkColumn = 10;
+    private int ResolutionColumn = 11;
+    private int ResolutionDateColumn = 12;
+    private int CreateDateColumn = 13;
+    private int UpdateDateColumn = 14;
 
     #endregion
 
@@ -60,7 +59,7 @@ public class IssuesWorksheetHandler : WorksheetExportHandlerBase
                 CurrentWorksheet.SetValue(currentRow, statusColumn, issue.Status);
                 CurrentWorksheet.SetValue(currentRow, typeColumn, issue.Type);
                 CurrentWorksheet.SetValue(currentRow, PriorityColumn, issue.Priority);
-                CurrentWorksheet.SetValue(currentRow, TimeSpentColumn, issue.TimeSpentInSeconds);
+                //CurrentWorksheet.SetValue(currentRow, TimeSpentColumn, issue.TimeSpentInSeconds);
                 CurrentWorksheet.SetValue(currentRow, EstimateTimeColumn, issue.TimeEstimateInSeconds);
                 //CurrentWorksheet.SetValue(currentRow, TimeLeftColumn, issue.TimeRemainingInSeconds);
                 CurrentWorksheet.SetValue(currentRow, ResolutionDateColumn, issue.ResolutionDate?.ToString("dd.MM.yy HH:mm"));
@@ -87,7 +86,7 @@ public class IssuesWorksheetHandler : WorksheetExportHandlerBase
             CurrentWorksheet.SetValue(currentRow, statusColumn, issue.Status);
             CurrentWorksheet.SetValue(currentRow, typeColumn, issue.Type);
             CurrentWorksheet.SetValue(currentRow, PriorityColumn, issue.Priority);
-            CurrentWorksheet.SetValue(currentRow, TimeSpentColumn, issue.TimeSpentInSeconds);
+            //CurrentWorksheet.SetValue(currentRow, TimeSpentColumn, issue.TimeSpentInSeconds);
             CurrentWorksheet.SetValue(currentRow, EstimateTimeColumn, issue.TimeEstimateInSeconds);
             //CurrentWorksheet.SetValue(currentRow, TimeLeftColumn, issue.TimeRemainingInSeconds);
             CurrentWorksheet.SetValue(currentRow, ResolutionDateColumn, issue.ResolutionDate?.ToString("dd.MM.yy HH:mm"));
@@ -109,11 +108,13 @@ public class IssuesWorksheetHandler : WorksheetExportHandlerBase
         CurrentWorksheet.SetValue(headerRow, sprintNameColumn, "Спринт");
         CurrentWorksheet.SetValue(headerRow, issueNameColumn, "Наименование задачи");
         CurrentWorksheet.SetValue(headerRow, issueKeyColumn, "Ключ задачи");
+        //CurrentWorksheet.Column(issueKeyColumn). Применить гиерссылку для ключа задачи
+
         CurrentWorksheet.SetValue(headerRow, lastExecutorColumn, "Последний исполнитель");
         CurrentWorksheet.SetValue(headerRow, statusColumn, "Статус");
         CurrentWorksheet.SetValue(headerRow, typeColumn, "Тип задачи");
         CurrentWorksheet.SetValue(headerRow, PriorityColumn, "Приоритет");
-        CurrentWorksheet.SetValue(headerRow, TimeSpentColumn, "Затрачено");
+        //CurrentWorksheet.SetValue(headerRow, TimeSpentColumn, "Затрачено");
         CurrentWorksheet.SetValue(headerRow, StoryPointsColumn, "Оценка SP");
         CurrentWorksheet.SetValue(headerRow, CountOfReworkColumn, "Количество доработок");
 
