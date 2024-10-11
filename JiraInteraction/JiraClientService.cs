3f3a32f585d1332a182d248a2bee52e419805981
d.sizova <daryasa1991@gmail.com>
2024-10-11 17:30:47 +0300

    Удалено лишнее

--- JiraInteraction/JiraClientService.cs
+++ JiraInteraction/JiraClientService.cs
@@ -10,8 +10,7 @@ public class JiraClientService : IJiraService
     public JiraClientService(JiraClientInitData initData)
     {
         var uri = "https://jira.bars.group";
-        // client = Jira.CreateRestClient(uri, initData.JiraLogin, initData.JiraPassword);
-        client = Jira.CreateRestClient(uri, "zabegaev", "1q2w3eSD");
+        client = Jira.CreateRestClient(uri, initData.JiraLogin, initData.JiraPassword);
         client.Issues.MaxIssuesPerRequest = 3000; 
     }
 
