dd8135397e75192e4ba44a0bd90e57db8e4a3f9b
Сизова Дарья <daryasa1991@mail.ru>
2026-06-19 13:56:25 +0300

    KPI 2 Вывод данных о затраченном времени на задачи

--- Reporter/WorkSheetsHandlers/KpiReport/Kpi2WorksheetHandler.cs
+++ Reporter/WorkSheetsHandlers/KpiReport/Kpi2WorksheetHandler.cs
@@ -55,47 +55,39 @@ public class Kpi2WorksheetHandler : WorksheetExportHandlerBase
 
         CurrentWorksheet.SetValue(currentRow, projectNameColumn, projectKey);
 
-        TimeSpan allResolvingTime = TimeSpan.FromSeconds(0);
+        TimeSpan allResolvingTime = TimeSpan.Zero;
         foreach (var issue in allClosedIssues)
         {
-            //надо посчитать сколько по времени задача была в к разработке от реализовано
-            //время когда задача попала от "к разработке" в "разработка"
-            var fromToDevelop = issue.ChangeLogs.Where(x => x.Items.Any(i =>
-                i.FieldName.ToLower() == JiraConstants.Status.ToLower()
-                && i.FromValue.ToLower() == JiraConstants.ToWork.ToLower()
-                && i.ToValue.ToLower() == JiraConstants.Work.ToLower())).First().CreateDate;
+            //Взять все переходы начало разработки и переходы в реализовано.
+            var statusChangedChangelogs = issue.ChangeLogs
+                .Where(x => x.Items.Any(
+                    ch => 
+                    ch.FieldName.ToLower() == JiraConstants.Status.ToLower()
+                    &&
+                    //Все переходы К разработке -> Разработка
+                    (ch.FromValue.ToLower() == JiraConstants.ToWork.ToLower() && ch.ToValue.ToLower() == JiraConstants.Work.ToLower()
+                    ||
+                    //Все переходы К доработке -> Доработка
+                    ch.FromValue.ToLower() == JiraConstants.ToRework.ToLower() && ch.ToValue.ToLower() == JiraConstants.Rework.ToLower()
+                    ||
+                    //Все переходы Ревью -> Реализован
+                    ch.FromValue.ToLower() == JiraConstants.Review.ToLower() && ch.ToValue.ToLower() == JiraConstants.Ready.ToLower()
+                    )))
+                //отсортировать по времени перехода
+                .OrderBy(ch => ch.CreateDate)
+                .ToArray();
 
-            //время от разработка в реализовано - за счет OrderBy и First это будет первый переход в реализовано
-            var fromReviewToReady = issue.ChangeLogs.Where(x => x.Items.Any(i =>
-                i.FieldName.ToLower() == JiraConstants.Status.ToLower()
-                && i.FromValue.ToLower() == JiraConstants.Review.ToLower()
-                && i.ToValue.ToLower() == JiraConstants.Ready.ToLower())).OrderBy(x => x.CreateDate).First().CreateDate;
-
-            //время чистого решения без доработок
-            var resolveWithoutReworkTime = fromToDevelop - fromReviewToReady;
-
-            //теперь надо считать время взятие в доработку - таких может быть много
-            var fromToRework = issue.ChangeLogs.Where(x => x.Items.Any(i =>
-                i.FieldName.ToLower() == JiraConstants.Status.ToLower()
-                && i.FromValue.ToLower() == JiraConstants.ToRework.ToLower()
-                && i.ToValue.ToLower() == JiraConstants.Rework.ToLower()));
-
-            //все переходы в реализовано - их много
-            var fromToReady = issue.ChangeLogs.Where(x => x.Items.Any(i =>
-                i.FieldName.ToLower() == JiraConstants.Status.ToLower()
-                && i.FromValue.ToLower() == JiraConstants.Review.ToLower()
-                && i.ToValue.ToLower() == JiraConstants.Ready.ToLower())).OrderBy(x => x.CreateDate);
-
-            //после этого надо как-то группировать переходы и считать время, после суммировать
-            if (resolveWithoutReworkTime == null)
+            for (int i = 0; i < statusChangedChangelogs.Count() -1; i++)
             {
-                continue;
+                var time = statusChangedChangelogs[i].CreateDate;
+                allResolvingTime += (statusChangedChangelogs[i+1].CreateDate - statusChangedChangelogs[i].CreateDate) ?? TimeSpan.Zero;
+                i++;
             }
-            allResolvingTime += resolveWithoutReworkTime!.Value; // + все что было по доработкам
         }
         CurrentWorksheet.SetValue(currentRow, totalClosedIssuesColumn, allClosedIssues.Count());
-        CurrentWorksheet.SetValue(currentRow, ResolveIssuesTimeColumn, allResolvingTime);
-        CurrentWorksheet.SetValue(currentRow, AverageResolvingTimeForIssues, allResolvingTime / allClosedIssues.Count());
+        CurrentWorksheet.SetValue(currentRow, ResolveIssuesTimeColumn, allResolvingTime.TotalHours);
+        var average = allResolvingTime / allClosedIssues.Count();
+        CurrentWorksheet.SetValue(currentRow, AverageResolvingTimeForIssues, average.TotalHours);
 
         CurrentWorksheet.Cells[currentRow, periodStartDateColumn].SetDateTime(_sprintReportEntity.ReportPeriod.StartDate);
         CurrentWorksheet.SetValue(currentRow, periodStartDateColumn, _sprintReportEntity.ReportPeriod.StartDate);
@@ -109,8 +101,8 @@ public class Kpi2WorksheetHandler : WorksheetExportHandlerBase
         //Заголовки данных
         CurrentWorksheet.SetValue(headerRow, projectNameColumn, "Проект");
         CurrentWorksheet.SetValue(headerRow, totalClosedIssuesColumn, "Количество задач");
-        CurrentWorksheet.SetValue(headerRow, ResolveIssuesTimeColumn, "Суммарное время решения задач");
-        CurrentWorksheet.SetValue(headerRow, AverageResolvingTimeForIssues, "Среднее время решения дефекта");
+        CurrentWorksheet.SetValue(headerRow, ResolveIssuesTimeColumn, "Суммарное время решения задач в часах");
+        CurrentWorksheet.SetValue(headerRow, AverageResolvingTimeForIssues, "Среднее время решения дефекта в часах");
 
         CurrentWorksheet.SetValue(headerRow, periodStartDateColumn, "Начало периода");
         CurrentWorksheet.SetValue(headerRow, periodEndDateColumn, "Конец периода");
